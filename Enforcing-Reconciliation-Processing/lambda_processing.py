14fc99f6b4aee6f45cc86c52b36ca3668ba58c02
Moin Ghori <moinghori.it@gmail.com>
2024-04-09 07:19:03 -0500

    Updated with Separe resource file and generated the report

--- Enforcing-Reconciliation-Processing/lambda_processing.py
+++ Enforcing-Reconciliation-Processing/lambda_processing.py
@@ -3,6 +3,7 @@ import json
 import requests
 from botocore.exceptions import ClientError
 import logging
+import os
 
 
 class LambdaProcessing():
@@ -10,6 +11,7 @@ class LambdaProcessing():
         self.user_tags = {}
 
     def get_lambda_functions_with_tags(self):
+        print(f"\nCalling Function:get_lambda_functions_with_tags ")
         try:
             lambda_function_with_tags = []
             client = boto3.client('lambda')
@@ -29,15 +31,18 @@ class LambdaProcessing():
 
 
     def call_leanix(self, res_type, acct_id):
-        print(f"Function: Call LeanIX API")
-        url = "http://localhost:5000/tags"
-        params = {
-            "resource_type": res_type,
-            "account_id": acct_id,
-        }
+        print(f"\nCalling Function:call_leanix ")
+        try:
+            url = "http://localhost:5000/tags"
+            params = {
+                "resource_type": res_type,
+                "account_id": acct_id,
+            }
 
-        response = requests.get(url, params=params)
-        return response.json()
+            response = requests.get(url, params=params)
+            return response.json()
+        except Exception as e:
+            print(f"Error Message : {e}")
 
     def load_user_tags(self):
         try:
@@ -47,65 +52,109 @@ class LambdaProcessing():
             print(f"Error Loading while fetching User Tags from file: Error : {e}")
 
     def validate_with_leanix_tags(self, leanIX_tags, resource_tags):
-        print(f"Function: validate_with_leanix_tags")
-        print(f"A201 : Resource Tags: {resource_tags}")
-        ix_tags = resource_tags.copy()
-        for ix_key, ix_val in leanIX_tags.items():
-            for resource_tags in ix_tags:
-                if resource_tags['Key'] == ix_key:
-                    resource_tags['Value'] = ix_val
-                    break
-            else:
-                ix_tags.append({'Key' : ix_key , 'Value' : ix_val})
+        print(f"\nCalling Function:validate_with_leanix_tags ")
+        try:
+            ix_tags = resource_tags.copy()
+            for ix_key, ix_val in leanIX_tags.items():
+                for resource_tags in ix_tags:
+                    if resource_tags['Key'] == ix_key:
+                        resource_tags['Value'] = ix_val
+                        break
+                else:
+                    ix_tags.append({'Key' : ix_key , 'Value' : ix_val})
 
-        # Converting List type into Dictionary type
-        aggregated_tags = {tag['Key']:tag['Value'] for tag in ix_tags}
-        print(f"Final Tags: {aggregated_tags}")
+            # Converting List type into Dictionary type
+            aggregated_tags = {tag['Key']:tag['Value'] for tag in ix_tags}
+            print(f"Final Tags: {aggregated_tags}")
 
-        return aggregated_tags
+            return aggregated_tags
+        except Exception as e:
+            print(f"Error Message : {e}")
 
 
     def tagging_reconciliation_lambda(self, res_id, final_tags):
-        client = boto3.client('lambda')
-        client.tag_resource(Resource=res_id, Tags=final_tags)
-        print(f"Tagging Enforcement for Lambda {res_id} has been completed. ")
+        print(f"\nCalling Function:tagging_reconciliation_lambda ")
+        try:
+            client = boto3.client('lambda')
+            client.tag_resource(Resource=res_id, Tags=final_tags)
+            print(f"Tagging Enforcement for Lambda {res_id} has been completed. ")
+        except Exception as e:
+            print(f"Error Message: {e}")
 
     def get_user_defined_tags(self, res_type,acct_id):
-        self.load_user_tags()
+        print(f"\nCalling Function:get_user_defined_tags ")
         try:
+            self.load_user_tags()
             return self.user_tags["user_tags"][res_type][acct_id]
         except Exception as e:
             print(f"Error: No user tags found for {res_type} and {acct_id} : Error_Message : {e}")
 
+    def display_dash(self,num):
+        print(num*"-")
+
+    def generate_report(self, res_type, funcName):
+        print(f"\nCalling Function: generate_report")
+        try:
+            filePath = "Reports/Complient_Resources_Report.json"
+
+            if os.path.exists(filePath):
+                with open("Reports/Complient_Resources_Report.json", "r") as file:
+                    report = json.load(file)
+            else:
+                report = {}
+
+            if res_type in report:
+                report[res_type][funcName] = "Complient"
+            else:
+                report[res_type] = {funcName: "Complient"}
+
+            json_str = json.dumps(report, indent=4)
+
+            with open("Reports/Complient_Resources_Report.json", "w") as file:
+                file.write(json_str)
+            print(f"Report has been generated.")
+            file.close()
+        except Exception as e:
+            print(f"Error Message : {e}")
+
     def lambda_processing(self,res_type):
-        print(f"Lambda Processing")
+        try:
+            self.display_dash(100)
+            print(f"Lambda Processing")
+            self.display_dash(100)
 
-        # Getting User Defined Tags
-        user_defined_tags = self.get_user_defined_tags(res_type, "B1")
-        print(f"User defined Tags for Lambda Functions are : {user_defined_tags}\n")
+            # Getting User Defined Tags
+            user_defined_tags = self.get_user_defined_tags(res_type, "B1")
+            print(f"User defined Tags for Lambda Functions are : {user_defined_tags}")
 
-        # Getting LeanIX Automation-Tags
-        leanix_tags = self.call_leanix(res_type, "B1")
-        print(f"LeanIX Automation Provided Tags: {leanix_tags}")
+            # Getting LeanIX Automation-Tags
+            leanix_tags = self.call_leanix(res_type, "B1")
+            print(f"LeanIX Automation Provided Tags: {leanix_tags}")
 
-        # Get Lambda functions and tags associated with it
-        cnt = 0
-        l_functions = self.get_lambda_functions_with_tags()
-        for funcName, funcARN, l_tags in l_functions:
-            cnt += 1
-            print(f"\nLambda Function : {funcName} and Tags : {l_tags}")
+            # Get Lambda functions and tags associated with it
+            cnt = 0
+            l_functions = self.get_lambda_functions_with_tags()
+            for funcName, funcARN, l_tags in l_functions:
+                print(f"Lambda Function : {funcName} and Tags : {l_tags}")
 
-            # Converting Dictionary type into List type
-            ll_tags = [{'Key':key, 'Value':value} for key,value in l_tags.items()]
+                # Converting Dictionary type into List type
+                ll_tags = [{'Key':key, 'Value':value} for key,value in l_tags.items()]
 
-            # Validate if Lambda Function has all the LeanIX tags or not.
-            aggregated_lix_tags = self.validate_with_leanix_tags(leanix_tags, ll_tags)
-            print(f"All User and Automated Tags are : {aggregated_lix_tags}")
+                # Validate if Lambda Function has all the LeanIX tags or not.
+                aggregated_lix_tags = self.validate_with_leanix_tags(leanix_tags, ll_tags)
+                print(f"All User and Automated Tags are : {aggregated_lix_tags}")
 
-            all_tags = {**aggregated_lix_tags, **user_defined_tags}
+                all_tags = {**aggregated_lix_tags, **user_defined_tags}
 
-            # Final Enforcing the all the mandatory tags to resource
-            self.tagging_reconciliation_lambda(funcARN, all_tags)
+                # Final Enforcing the all the mandatory tags to resource
+                self.tagging_reconciliation_lambda(funcARN, all_tags)
+                cnt += 1
+                self.generate_report(res_type, funcName)
+                self.display_dash(100)
+
+            print(f"\nNumber of Lambda Function Processed : {cnt}\n")
+        except Exception as e:
+            print(f"Error Message : {e}")
 
 
 
--- Enforcing-Reconciliation-Processing/s3_processing.py
+++ Enforcing-Reconciliation-Processing/s3_processing.py
@@ -3,6 +3,7 @@ import json
 import requests
 from botocore.exceptions import ClientError
 import logging
+import os
 
 
 class S3Processing():
@@ -10,6 +11,7 @@ class S3Processing():
         self.user_tags = {}
 
     def get_s3_functions_with_tags(self):
+        print(f"\nCalling Function:get_s3_functions_with_tags ")
         try:
             s3_buckets_with_tags = []
             client = boto3.client('s3')
@@ -30,15 +32,18 @@ class S3Processing():
         return s3_buckets_with_tags
 
     def call_leanix(self, res_type, acct_id):
-        print(f"Function: Call LeanIX API")
-        url = "http://localhost:5000/tags"
-        params = {
-            "resource_type": res_type,
-            "account_id": acct_id,
-        }
+        print(f"\nCalling Function:call_leanix ")
+        try:
+            url = "http://localhost:5000/tags"
+            params = {
+                "resource_type": res_type,
+                "account_id": acct_id,
+            }
 
-        response = requests.get(url, params=params)
-        return response.json()
+            response = requests.get(url, params=params)
+            return response.json()
+        except Exception as e:
+            print(f"Error Message : {e}")
 
     def load_user_tags(self):
         try:
@@ -48,67 +53,119 @@ class S3Processing():
             print(f"Error Loading while fetching User Tags from file: Error : {e}")
 
     def validate_with_leanix_tags(self, leanIX_tags, resource_tags):
-        print(f"Function: validate_with_leanix_tags")
-        print(f"A201 : Resource Tags: {resource_tags}")
-        ix_tags = resource_tags.copy()
-        for ix_key, ix_val in leanIX_tags.items():
-            for resource_tags in ix_tags:
-                if resource_tags['Key'] == ix_key:
-                    resource_tags['Value'] = ix_val
-                    break
-            else:
-                ix_tags.append({'Key' : ix_key , 'Value' : ix_val})
+        print(f"\nCalling Function:validate_with_leanix_tags ")
+        try:
+            ix_tags = resource_tags.copy()
+            for ix_key, ix_val in leanIX_tags.items():
+                for resource_tags in ix_tags:
+                    if resource_tags['Key'] == ix_key:
+                        resource_tags['Value'] = ix_val
+                        break
+                else:
+                    ix_tags.append({'Key' : ix_key , 'Value' : ix_val})
 
-        # Converting List type into Dictionary type
-        aggregated_tags = {tag['Key']:tag['Value'] for tag in ix_tags}
-        print(f"Final Tags: {aggregated_tags}")
+            # Converting List type into Dictionary type
+            aggregated_tags = {tag['Key']:tag['Value'] for tag in ix_tags}
+            print(f"Final Tags: {aggregated_tags}")
 
-        return aggregated_tags
+            return aggregated_tags
+        except Exception as e:
+            print(f"Error Message : {e}")
 
     def tagging_reconciliation_s3(self, res_id, final_tags):
-        client = boto3.client('s3')
-        tagging = {'TagSet': final_tags}
-        client.put_bucket_tagging(Bucket=res_id, Tagging=tagging)
-        print(f"Tagging Enforcement for bucket {res_id} has been completed. \n")
+        print(f"\nCalling Function:tagging_reconciliation_s3 ")
+        try:
+            client = boto3.client('s3')
+            tagging = {'TagSet': final_tags}
+            client.put_bucket_tagging(Bucket=res_id, Tagging=tagging)
+            print(f"Tagging Enforcement for bucket {res_id} has been completed. \n")
+        except Exception as e:
+            print(f"Error Message: {e}")
+
 
 
     def get_user_defined_tags(self, res_type,acct_id):
-        self.load_user_tags()
+        print(f"\nCalling Function:get_user_defined_tags ")
         try:
+            self.load_user_tags()
             return self.user_tags["user_tags"][res_type][acct_id]
         except Exception as e:
             print(f"Error: No user tags found for {res_type} and {acct_id} : Error_Message : {e}")
 
+    def display_dash(self,num):
+        print(num*"-")
+
+
+    def generate_report(self,res_type,bucket):
+        print(f"Calling Function: generate_report")
+        try:
+            filePath = "Reports/Complient_Resources_Report.json"
+
+            if os.path.exists(filePath):
+                with open("Reports/Complient_Resources_Report.json", "r") as file:
+                    report = json.load(file)
+            else:
+                report = {}
+
+
+            if res_type in report:
+                report[res_type][bucket] = "Complient"
+            else:
+                report[res_type] = {bucket:"Complient"}
+
+            json_str = json.dumps(report,indent=4)
+
+            with open("Reports/Complient_Resources_Report.json","w") as file:
+                file.write(json_str)
+
+            file.close()
+            print(f"Report has been generated")
+        except Exception as e:
+            print(f"Error Message : {e}")
+
+
+
     def s3_processing(self,res_type):
-        print(f"S3 Bucket Processing")
+        try:
+            self.display_dash(100)
+            print(f"S3 Bucket Processing")
+            self.display_dash(100)
 
-        # Getting User Defined Tags
-        user_defined_tags = self.get_user_defined_tags(res_type, "A1")
-        print(f"User defined Tags for S3 buckets are : {user_defined_tags}")
+            # Getting User Defined Tags
+            user_defined_tags = self.get_user_defined_tags(res_type, "A1")
+            print(f"User defined Tags for S3 buckets are : {user_defined_tags}")
 
-        # Getting LeanIX Automation-Tags
-        leanix_tags = self.call_leanix("s3", "A1")
-        print(f"LeanIX Automation Provided Tags: {leanix_tags}")
+            # Getting LeanIX Automation-Tags
+            leanix_tags = self.call_leanix("s3", "A1")
+            print(f"LeanIX Automation Provided Tags: {leanix_tags}")
 
-        # Get S3 Buckets and tags associated with it
-        cnt = 0
-        s3_buckets = self.get_s3_functions_with_tags()
-        for bucket,s3_tags in s3_buckets:
-            # Getting S3 buckets Tags
-            #s3_tags = obj.get_bucket_tags(bucket)
+            # Get S3 Buckets and tags associated with it
+            cnt = 0
+            s3_buckets_with_tags = self.get_s3_functions_with_tags()
+            for bucket,s3_tags in s3_buckets_with_tags:
+                print(f"Bucket Name: {bucket}")
 
-            # Validate if S3 Bucket has all the LeanIX tags or not.
-            aggregated_s3ix_tags = self.validate_with_leanix_tags(leanix_tags, s3_tags)
-            print(f"All Mandatory Tags are : {aggregated_s3ix_tags}")
+                # Validate if S3 Bucket has all the LeanIX tags or not.
+                aggregated_s3ix_tags = self.validate_with_leanix_tags(leanix_tags, s3_tags)
+                print(f"All Existing & Automation Tags: {aggregated_s3ix_tags}")
 
-            all_tags = {**aggregated_s3ix_tags, **user_defined_tags}
+                all_tags = {**aggregated_s3ix_tags, **user_defined_tags}
 
-            # Converting Dictionary type into List type
-            final_tags = [{'Key':key, 'Value':value} for key,value in all_tags.items()]
-            print(f"All User and Automated Tags are : {final_tags}")
+                # Converting Dictionary type into List type
+                final_tags = [{'Key':key, 'Value':value} for key,value in all_tags.items()]
+                print(f"All Existing,User-defined & Automated Tags: {final_tags}")
 
-            # Final Enforcing the all the mandatory tags to resource
-            self.tagging_reconciliation_s3(bucket, final_tags)
+                # Final Enforcing the all the mandatory tags to resource
+                self.tagging_reconciliation_s3(bucket, final_tags)
+                cnt += 1
+                self.generate_report(res_type,bucket)
+                self.display_dash(100)
+
+            print(f"\nNumber of S3 Buckets Processed : {cnt}\n")
+        except Exception as e:
+            print(f"Error Message : {e}")
+
+
 
 
 
